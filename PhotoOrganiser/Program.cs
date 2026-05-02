f7c7c00375c2d90e6cf2aa5d0cea107703771fcc
Andy Blackledge <40278654+andybalham@users.noreply.github.com>
2026-05-02 16:31:44 +0100

    Added ex catching

--- PhotoOrganiser/Program.cs
+++ PhotoOrganiser/Program.cs
@@ -7,6 +7,12 @@ static class Program
     [STAThread]
     static void Main()
     {
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+        Application.ThreadException += (_, e) =>
+            MessageBox.Show(e.Exception.ToString(), "Unhandled Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+            MessageBox.Show(e.ExceptionObject?.ToString(), "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
         ApplicationConfiguration.Initialize();
         Application.Run(new MainForm());
     }
